5b4d746f32e2134c827c3ce9ac127b150258969b
Tejaa Chintaluri <saitejaac@gmail.com>
2023-09-22 09:08:53 +0530

    bookstore: add TestBuyErrorsIfNoCopiesLeft and updated Buy function

--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -22,9 +22,25 @@ func TestBuy(t *testing.T) {
 		Copies: 5,
 	}
 	want := 4
-	bought := bookstore.Buy(b)
+	bought, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
 	got := bought.Copies
 	if want != got {
 		t.Errorf("want %d copies after buying 1 copy from a stock of %d copies, got %d", want, b.Copies, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Marie Kondo",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("want error buying from zero copies, got nil")
 	}
 }
